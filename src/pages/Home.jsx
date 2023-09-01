1fa2e78b81123c4f02cf71ae855fd3a54b10c0e9
Zahra Shahabi <zahra.shahabi.dk@gmail.com>
2023-09-01 12:35:17 +0200

    Add RegisterEmail and Agent parts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,23 @@ import SecondHeader from "../templates/SecondHeader";
 import Hero from "../templates/Hero";
 import Introduction from "../templates/Introduction";
 import ErrorBoundary from "../components/ErrorBoundary";
-import SelectBuildings from "../components/SelectBuildings";
+import SelectBuildings from "../templates/SelectBuildings";
+import RegisterEmail from "../components/RegisterEmail";
+import Agent from "../templates/Agent";
 
 const Home = () => {
-    return(
-        <>
-            <ErrorBoundary>
-                <FirstHeader />
-                <SecondHeader />
-                <Hero />
-                <Introduction />
-                <SelectBuildings />
-            </ErrorBoundary>
-            
-        </>
-    )
-}
+  return (
+    <>
+      <ErrorBoundary>
+        <FirstHeader />
+        <SecondHeader />
+        <Hero />
+        <Introduction />
+        <SelectBuildings />
+        <RegisterEmail />
+        <Agent />
+      </ErrorBoundary>
+    </>
+  );
+};
 export default Home;
